4f6b4288af9e54070792c8e22290e83b5798105f
Jon Eubank <joneubank@gmail.com>
2025-05-19 15:55:53 -0400

    Update to sketch 2.0.0-beta.2 and modern React render

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from '@code-not-art/sketch';
+import { FullPageSketchCanvas } from '@code-not-art/sketch';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Helmet } from 'react-helmet';
 import sketch from './sketch';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App sketch={sketch} />
-	</React.StrictMode>,
-	root,
+const App = () => (
+	<>
+		<Helmet>
+			<title>Code Sketch</title>
+			<meta name="description" content="HTML Canvas sketch made by code" />
+		</Helmet>
+		<FullPageSketchCanvas sketch={sketch}></FullPageSketchCanvas>
+	</>
+);
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
 );
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,101 +1,71 @@
+import { Constants, Utils } from '@code-not-art/core';
 import {
-	ControlPanel,
-	Parameters,
-	Sketch,
-	SketchConfig,
-	SketchDraw,
-	SketchInit,
+	type ControlPanelElements,
+	createSketch,
+	createSketchConfig,
+	type SketchDraw,
+	type SketchInit,
 	SketchLoop,
 	SketchReset,
-	type ControlPanelElements,
 } from '@code-not-art/sketch';
 
-const config = SketchConfig({});
+const { array, repeat } = Utils;
+const { TAU } = Constants;
 
-const controls = {
-	exampleControls: ControlPanel('Example Custom Controls', {
-		exampleBoolean: Parameters.boolean({ label: 'Example Bool' }),
-		exampleNumber: Parameters.number({ label: 'Example Number' }),
-		exampleRange: Parameters.range({
-			label: 'Example Range',
-			initialValue: [0.25, 0.75],
-		}),
-		exampleSelect: Parameters.multiSelect({
-			label: 'Example Select',
-			options: ['A', 'B', 'C'],
-		}),
-		exampleString: Parameters.string({ label: 'Example String' }),
-	}),
-} satisfies ControlPanelElements;
+const config = createSketchConfig({
+	menuDelay: 0,
+});
 
-type CustomParams = typeof controls;
+const controls = {} satisfies ControlPanelElements;
+
+type CustomControls = typeof controls;
 type CustomData = {};
 
 /**
  * Init is run once on startup to initialize the values of the sketch data.
- * Since init is only run once, when the sketch is first loaded, this can be
- * used to run any calculations that are slow and you don't want to repeat on
- * redraw.
- *
- * The content of the data object returned by this function will persist across
- * every draw and every step of the loop. You are free to modify and update this
- * data in the draw and loop steps, just be aware that it will never be reset
- * by the Sketch lifecycle.
- *
- * @param sketchProps Access to canvas context, RNG, color pallete, parameter
- * values, and persistent data.
+ * Since init is only run once, when the sketch is first loaded, this can be used to run any calculations that are slow and you don't want to repeat on redraw.
+ * @param sketchProps Access to canvas context, RNG, color pallete, parameter values, and persistent data
  */
-const init: SketchInit<CustomParams, CustomData> = (props) => {
+const init: SketchInit<CustomControls, CustomData> = (props) => {
 	console.log('Initializing Sketch...');
 	const initialData: CustomData = {};
 	return initialData;
 };
 
 /**
- * Reset does not run the first time the sketch is drawn, instead it is run
- * between redraws of the sketch. This can be used to reset the data in the
- * sketch props that is passed to the draw and loop methods. The current data
- * state is provided, so if you want to persist any data from one drawing to
- * the next redraw you can configure that here.
+ * Reset does not run the first time the sketch is drawn, instead it is run between redraws of the sketch.
+ * This can be used to reset the data in the sketch props that is passed to the draw and loop methods.
+ * The current data state is provided, so if you want to persist any data from one drawing to the next redraw you can configure that here.
  *
- * @param sketchProps Access to canvas context, RNG, color pallete, parameter
- * values, and persistent data.
+ * Note that there is a default reset function that will be run if you do not provide a custom reset. That reset function will clear the canvas and persist the previous drawing's data.
+ * If you want to modify the persistent data or prevent the clear from running you should provide your own reset.
+ * @param sketchProps Access to canvas context, RNG, color pallete, parameter values, and persistent data
  */
-const reset: SketchReset<CustomParams, CustomData> = (props, data) => {
+const reset: SketchReset<CustomControls, CustomData> = (props, data) => {
 	console.log('Resetting Sketch...');
 	const resetData: CustomData = {};
 	return resetData;
 };
 
 /**
- * Runs once for the sketch, after data initialization/reset and before the
- * animation loop begins.
- *
- * @param sketchProps Access to canvas context, RNG, color pallete, parameter
- * values, and persistent data.
+ * Runs once for the sketch, after data initialization and before the animation loop begins.
+ * @param sketchProps Access to canvas context, RNG, color pallete, parameter values, and persistent data
  */
-const draw: SketchDraw<CustomParams, CustomData> = ({ canvas, palette, params, rng }, data) => {
+const draw: SketchDraw<CustomControls, CustomData> = ({ canvas, palette, params, rng }, _data) => {
 	console.log('Drawing Sketch...');
-
-	// Random canvas background color
-	canvas.fill(palette.colors[0]);
-
-	const {
-		exampleControls: { exampleBoolean, exampleNumber, exampleRange, exampleSelect, exampleString },
-	} = params;
 
 	// Your sketch instructions here:
 	// ...
+	canvas.fill(palette.colors[0]);
 };
 
 /**
- * Repeats on every available animation frame. Attemtps to render on every 1/60th of a second but may be longer.
- * The frame data will provide specific timing data if frame windows are missed.
+ * Repeats on every available animation frame. Attemtps to render on every 1/60th of a second but the frame data will provide specific timing data if frame windows are missed.
  * @param sketchProps Access to canvas context, RNG, color pallete, parameter values, and persistent data
  * @param frameData Frame count, and time since last frame was drawn.
  * @returns return value indicates if the loop is complete: return true when finished. This will be called repeatedly so long as it returns false.
  */
-const loop: SketchLoop<CustomParams, CustomData> = ({}, _data, { frame }) => {
+const loop: SketchLoop<CustomControls, CustomData> = ({}, _data, { frame }) => {
 	console.log(`Sketch animation loop, frame ${frame} ...`);
 
 	// Your sketch animation instructions here:
@@ -105,11 +75,16 @@ const loop: SketchLoop<CustomParams, CustomData> = ({}, _data, { frame }) => {
 	return true;
 };
 
-export default Sketch<CustomParams, CustomData>({
+export default createSketch<CustomControls, CustomData>({
 	config,
 	controls,
 	init,
 	draw,
 	loop,
 	reset,
+});
+
+const exampleSketch = createSketch({
+	init: () => ({}),
+	controls: {},
 });
